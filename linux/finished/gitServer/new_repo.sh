b22781c8fe5c8c9937c4c924f3728055bc007b17
Tyler Hoyt <tylerhoyt93@gmail.com>
2015-04-27 12:39:34 -0400

    Fixed add_trash_alias and deleted gitserverscripts

--- linux/finished/gitServer/new_repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $UID != 1001 ]]; then
-    echo "Please run this script as git:"
-    exit 1
-fi
-
-/bin/mkdir ~/$1.git
-/bin/git init --bare ~/$1.git
--- linux/finished/gitServer/remove_repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $UID != 1001 ]]; then
-    echo "Please run this script as git:"
-    exit 1
-fi
-
-rm -rf ~/$1.git
