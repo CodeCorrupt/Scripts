5bcd12b3e065049a743be2d953008fc8e4c4ffbd
Tyler Hoyt <tylerhoyt93@gmail.com>
2015-06-29 11:58:46 -0400

    Site codes case insensitive. Added -l flag. completion The -l flag lists the connections. I've also added a README and updated the installer.sh

--- linux/sign_on/installer.sh
+++ linux/sign_on/installer.sh
@@ -2,10 +2,15 @@
 
 ## This is the installer to allow access to the site script within the PATH variable
 ## HOW TO RUN:
-##		/tmp/sign_on/installer.sh
+##		/support/site_login/installer.sh
 ##		Then exit out and log back in
 
 
 echo "" >> ~/.bashrc
 echo "# The path to the site script to allow easy access to terminals" >> ~/.bashrc
-echo 'PATH=/tmp/sign_on:$PATH' >> ~/.bashrc
+echo "#This allows the script to be called without pathing out the full name" >> ~/.bashrc
+echo "PATH=$(dirname "$(readlink -f "$0")"):\$PATH" >> ~/.bashrc
+echo "#This enables bash completion so you can tab complete the site name" >> ~/.bashrc
+echo "source $(dirname "$(readlink -f "$0")")/site-comp" >> ~/.bashrc
+echo "Added the script to the path in your ~/.bashrc. "
+echo "Please log out and back in and you will be able to run site \\site code\\"
